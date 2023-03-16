48b9ee231b1a46561339b4ffcee3a2d248b8ed26
dariorod <dario2303@gmail.com>
2023-03-16 11:29:18 -0300

    finished project

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ const closeMenuBtn = document.querySelector('.nav-mobile__close');
 const openMenuBtn = document.querySelector('.header__menu');
 const navMobile = document.querySelector('.nav-mobile');
 
+const h = navMobile.style;
+h.height = '0';
 
 menu()
 function menu() {
+
     openMenuBtn.addEventListener('click', e => {
         if(e.isTrusted) {            
-            navMobile.style.height = '100vh';
+            h.height = '100vh';
         }
     })
     closeMenuBtn.addEventListener('click', e => {
         if(e.isTrusted) {
-            navMobile.style.height = '0';
+            h.height = '0';
+        }
+    })
+}
+
+resizing()
+function resizing () {
+    window.addEventListener('resize', () => {
+        if (innerWidth > 375) {
+            h.height = '0';
         }
     })
 }
